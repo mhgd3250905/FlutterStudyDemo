8451bcdf7acb3d5e29b9f7160f5b3cd4d9581f07
IMT9FJBH3SVCXBQ\Administrator <294851575@qq.com>
2018-10-17 23:27:51 +0800

    1.20181017笔记：有状态的widget

--- lib/main.dart
+++ lib/main.dart
@@ -27,10 +27,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          new Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
+          new FavoriteWidget(),
         ],
       ),
     );
@@ -113,8 +110,43 @@ class FavoriteWidget extends StatefulWidget{
 }
 
 class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited=true;
+  int _favoriteCount=41;
+
+  void _toggleFavorite(){
+    setState(() {
+      if(_isFavorited){
+        _favoriteCount-=1;
+        _isFavorited=false;
+      }else{
+        _favoriteCount+=1;
+        _isFavorited=true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    return new Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new Container(
+          padding: const EdgeInsets.all(0.0),
+          child: new IconButton(
+            icon: (_isFavorited
+            ?new Icon(Icons.star)
+            :new Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        new SizedBox(
+          width: 18.0,
+          child: new Container(
+            child: new Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
   }
 }
